08b891b8a52217e55a60cc881e83f819945de408
ARSON <56494633+ARSON-more@users.noreply.github.com>
2022-05-19 16:32:36 +0100

    fin

--- src/index.java
+++ src/index.java
@@ -1,45 +1,38 @@
-import java.util.ArrayList;
+//import java.util.*;
 
 public class index {
-    static String input = "r";
+    static char input = 'r';
 
-    public static boolean check_input(String input) {
+    public static boolean check_input(char input) {
 
         System.out.print(input);
         boolean temp_valid = false;
         try {
             switch (input) {
-                case "Y":
+                case 'Y':
                     temp_valid = true;
-                    System.out.println("what");
+                    System.out.println('Y');
                     break;
 
-                case "R":
+                case 'R':
                     temp_valid = true;
-                    System.out.print("hi");
-                    break;
-
-                case "penis":
-                    temp_valid = true;
+                    System.out.print('R');
                     break;
 
                 default:
-                    System.out.println(" invalid input");
+                    System.out.println("invalid input");
                     temp_valid = false;
 
             }
 
         } catch (Exception e) {
+            return false;
 
         }
         return temp_valid;
     }
 
-    public static void create_empty_table() {
-
-    }
-
-    public static boolean validate_board(String[][] grid) {
+    public static boolean validate_board(char[][] grid) {
         boolean valid_grid = false;
         int counter_y = 0;
         int counter_R = 0;
@@ -47,10 +40,10 @@ public class index {
 
             for (int B = 0; B < 7; B++) {
                 switch (grid[A][B]) {
-                    case "Y":
+                    case 'Y':
                         counter_y++;
                         break;
-                    case "R":
+                    case 'R':
                         counter_R++;
                         break;
                     default:
@@ -61,7 +54,7 @@ public class index {
         }
 
         if (counter_R < counter_y) {
-            System.out.println("too many yellow ");
+            System.out.println("too many yellow");
             return false;
         }
         if (counter_R >= counter_y + 2) {
@@ -77,15 +70,15 @@ public class index {
                 try {
                     switch (grid[i][x]) {
 
-                        case ".":
+                        case '.':
                             valid_grid = true;
                             break;
 
-                        case "R":
+                        case 'R':
                             valid_grid = true;
 
                             if (i - 1 < 4) {
-                                if (grid[i + 1][x] == ".") {
+                                if (grid[i + 1][x] == '.') {
                                     System.out.println("impossible config");
                                     // System.out.print(i);
 
@@ -96,12 +89,12 @@ public class index {
 
                             break;
 
-                        case "Y":
+                        case 'Y':
                             valid_grid = true;
                             counter_y++;
 
                             if (i - 1 < 4) {
-                                if (grid[i + 1][x] == ".") {
+                                if (grid[i + 1][x] == '.') {
                                     System.out.println("impossible config");
                                     // System.out.print(i);
 
@@ -126,17 +119,17 @@ public class index {
 
             }
 
-            // System.out.print("\n");
+            // System.out.print('\n');
         }
 
         return valid_grid;
     }
 
-    public static boolean validate_move(int input_col, String[][] grid) {
+    public static boolean validate_move(int input_col, char[][] grid) {
         boolean valid_move = false;
         // String area = grid[4][1];
         try {
-            if (grid[0][input_col] == ".") {
+            if (grid[0][input_col] == '.') {
                 System.out.println("valid move");
                 valid_move = true;
 
@@ -151,15 +144,19 @@ public class index {
         return valid_move;
     }
 
-    public static boolean all_valid_moves(String[][] grid) {
-        boolean valid_move = false;
+    public static ArrayList<Integer> all_valid_moves(char[][] grid) {
+        // boolean valid_move = false;
+        ArrayList<Integer> rows = new ArrayList<Integer>();
+        // numbers.add(10);
+
         // String area = grid[4][1];
         try {
 
-            for (int t = 0; t < 6; t++) {
-                if (grid[0][t] == ".") {
+            for (int t = 0; t <= 6; t++) {
+                if (grid[0][t] == '.') {
                     System.out.println("valid move" + t);
-                    valid_move = true;
+                    // valid_move = true;
+                    rows.add(t);
 
                 } else {
                     System.out.println("full");
@@ -171,23 +168,23 @@ public class index {
             System.out.println("invalid input form");
 
         }
-
-        return valid_move;
+        System.out.print(rows);
+        return (rows);
     }
 
-    public static String whos_turn(String[][] grid) {
+    public static char whos_turn(char[][] grid) {
         boolean valid_grid = true;
         int counter_y = 0;
         int counter_R = 0;
-        String move = "";
+        char move = ' ';
         for (int A = 0; A < 6; A++) {
 
             for (int B = 0; B < 7; B++) {
                 switch (grid[A][B]) {
-                    case "Y":
+                    case 'Y':
                         counter_y++;
                         break;
-                    case "R":
+                    case 'R':
                         counter_R++;
                         break;
                     default:
@@ -199,11 +196,11 @@ public class index {
 
         if (counter_R < counter_y) {
             System.out.println("too many yellow ");
-            return ".";
+            return '.';
         }
         if (counter_R >= counter_y + 2) {
             System.out.println("too many reds impossible");
-            return ".";
+            return '.';
         }
 
         System.out.println("yellow moves = " + counter_y + "\n" + "red moves=" + counter_R);
@@ -216,36 +213,37 @@ public class index {
                     if (valid_grid) {
                         switch (grid[i][x]) {
 
-                            case ".":
+                            case '.':
                                 valid_grid = true;
                                 break;
 
-                            case "R":
+                            case 'R':
                                 valid_grid = true;
 
                                 if (i - 1 < 4) {
-                                    if (grid[i + 1][x] == ".") {
+                                    if (grid[i + 1][x] == '.') {
                                         System.out.println("impossible config");
                                         // System.out.print(i);
 
                                         valid_grid = false;
-                                        break;
+                                        return '.';
+
                                     }
 
                                 }
 
                                 break;
 
-                            case "Y":
+                            case 'Y':
                                 valid_grid = true;
 
                                 if (i - 1 < 4) {
-                                    if (grid[i + 1][x] == ".") {
+                                    if (grid[i + 1][x] == '.') {
                                         System.out.println("impossible config");
                                         // System.out.print(i);
 
                                         valid_grid = false;
-                                        break;
+                                        return '.';
                                     }
                                 }
 
@@ -266,15 +264,15 @@ public class index {
 
             }
 
-            // System.out.print("\n");
+            // System.out.print('\n');
         }
 
         try {
 
             if (valid_grid && counter_y == counter_R) {
-                move = "R";
+                move = 'R';
             } else if (valid_grid && counter_R > counter_y) {
-                move = "Y";
+                move = 'Y';
             }
 
         } catch (Exception e) {
@@ -285,142 +283,151 @@ public class index {
         return move;
     }
 
-    public static String winnder(String[][] grid) {
-
-        String winner = "";
-        boolean won = false;
-        // String area = grid[4][1];
-        int wincheck_y = 0;
-        int wincheck_r = 0;
-        try {
-
-            for (int A = 0; A < 6; A++) {
-                wincheck_y = 0;
-                wincheck_r = 0;
-
-                for (int B = 0; B < 7; B++) {
-                    switch (grid[A][B]) {
-                        case "Y":
-                            wincheck_y++;
-                            wincheck_r = 0;
-
-                            if (wincheck_y == 4) {
-                                winner = "Y";
-                                won = true;
-                                System.out.println(winner);
-
-                                return winner;
-                            }
-
-                            break;
-                        case "R":
-                            wincheck_r++;
-                            wincheck_y = 0;
-                            if (wincheck_r == 4) {
-                                winner = "R";
-                                System.out.println(winner);
-                                won = true;
-
-                                return winner;
-                            } else {
-                                wincheck_y = 0;
-                            }
-                            break;
-
-                        default:
-                            wincheck_y = 0;
-                            wincheck_r = 0;
-                            break;
-                    }
-                }
-
-            }
-            /////////////////
-            System.out.println("checking next");
-
-            for (int B = 0; B < 7; B++) {
-
-                wincheck_y = 0;
-                wincheck_r = 0;
-
-                for (int A = 0; A < 6; A++) {
-                    switch (grid[A][B]) {
-                        case "Y":
-                            wincheck_y++;
-                            wincheck_r = 0;
-
-                            if (wincheck_y == 4) {
-                                winner = "Y";
-                                won = true;
-                                System.out.println(winner);
-
-                                return winner;
-                            }
-
-                            break;
-                        case "R":
-                            wincheck_r++;
-                            wincheck_y = 0;
-                            if (wincheck_r == 4) {
-                                winner = "R";
-                                System.out.println(winner);
-                                won = true;
-
-                                return winner;
-                            } else {
-                                wincheck_y = 0;
-                            }
-                            break;
-
-                        default:
-                            wincheck_y = 0;
-                            wincheck_r = 0;
-                            break;
-                    }
-                }
-
-            }
-
-        } catch (Exception e) {
-            System.out.println("invalid input form");
-
-        }
-
-        return "failed";
-    }
+    /*
+     * public static char winnder(char[][] grid) {
+     * 
+     * char winner = ' ';
+     * boolean won = false;
+     * // String area = grid[4][1];
+     * int wincheck_y = 0;
+     * int wincheck_r = 0;
+     * try {
+     * 
+     * for (int A = 0; A < 6; A++) {
+     * wincheck_y = 0;
+     * wincheck_r = 0;
+     * 
+     * for (int B = 0; B < 7; B++) {
+     * switch (grid[A][B]) {
+     * case 'Y':
+     * wincheck_y++;
+     * wincheck_r = 0;
+     * 
+     * if (wincheck_y == 4) {
+     * winner = 'Y';
+     * won = true;
+     * System.out.println(winner);
+     * 
+     * return winner;
+     * }
+     * 
+     * break;
+     * case 'R':
+     * wincheck_r++;
+     * wincheck_y = 0;
+     * if (wincheck_r == 4) {
+     * winner = 'R';
+     * System.out.println(winner);
+     * won = true;
+     * 
+     * return winner;
+     * } else {
+     * wincheck_y = 0;
+     * }
+     * break;
+     * 
+     * default:
+     * wincheck_y = 0;
+     * wincheck_r = 0;
+     * break;
+     * }
+     * }
+     * 
+     * }
+     * /////////////////
+     * System.out.println("checking next");
+     * 
+     * for (int B = 0; B < 7; B++) {
+     * 
+     * wincheck_y = 0;
+     * wincheck_r = 0;
+     * 
+     * for (int A = 0; A < 6; A++) {
+     * switch (grid[A][B]) {
+     * case 'Y':
+     * wincheck_y++;
+     * wincheck_r = 0;
+     * 
+     * if (wincheck_y == 4) {
+     * winner = 'Y';
+     * won = true;
+     * System.out.println(winner);
+     * 
+     * return winner;
+     * }
+     * 
+     * break;
+     * case 'R':
+     * wincheck_r++;
+     * wincheck_y = 0;
+     * if (wincheck_r == 4) {
+     * winner = 'R';
+     * System.out.println(winner);
+     * won = true;
+     * 
+     * return winner;
+     * } else {
+     * wincheck_y = 0;
+     * }
+     * break;
+     * 
+     * default:
+     * wincheck_y = 0;
+     * wincheck_r = 0;
+     * break;
+     * }
+     * }
+     * 
+     * }
+     * 
+     * } catch (Exception e) {
+     * System.out.println("invalid input form");
+     * 
+     * }
+     * 
+     * return 'failed';
+     * }
+     */
 
     public static void main(String[] args) {
         // red always starts
 
-        String[][] grid = { { ".", ".", ".", "R", "R", "R", "R" },
-                { ".", ".", ".", ".", ".", ".", "." },
-                { ".", ".", ".", ".", ".", ".", "." },
-                { ".", ".", ".", ".", ".", ".", "." },
-                { ".", ".", ".", ".", ".", ".", "." },
-                { ".", ".", ".", ".", ".", ".", "." },
+        char[][] grid = {
+                { '.', '.', '.', '.', '.', '.', '.' },
+                { '.', '.', '.', '.', '.', '.', '.' },
+                { '.', '.', '.', '.', '.', '.', '.' },
+                { '.', '.', '.', '.', '.', '.', '.' },
+                { '.', '.', '.', '.', '.', '.', '.' },
+                { '.', '.', 'R', '.', '.', '.', '.' },
         };
-
-        // grid[5][1] = "R";
-        // grid[4][1] = "Y";
-        // grid[3][1] = "R";
-        // grid[2][1] = "Y";
-        // grid[1][1] = "R";
-        // grid[0][1] = "Y";
 
         for (int i = 0; i < 6; i++) {
 
             for (int x = 0; x < 7; x++) {
                 System.out.print(grid[i][x]);
-                System.out.print(" ");
+                System.out.print(' ');
             }
-            System.out.print("\n");
+            System.out.print('\n');
         }
 
+        /*
+         * if ((grid.length == 6) && (grid[0].length == 7)) {
+         * System.out.println("Valid board"); }
+         * else {
+         * System.out.println("Invalid board");
+         * }
+         */
+
+        /*
+         * int test = grid[0].length;
+         * System.out.println(test);
+         */
+
         // validate_board(grid);
-        // validate_move(1,grid);
-        // all_valid_moves(grid);
+        // validate_move(5,grid);
+        all_valid_moves(grid);
         // whos_turn(grid);
-        winnder(grid);
+        // winnder(grid);
 
     }
 
